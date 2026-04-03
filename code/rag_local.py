02cf86dae4a250c67af8b0851c6c4f01fa80464c
Stefan Weil <sw@weilnetz.de>
2026-04-03 20:33:56 +0200

    Fix import statement
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- code/rag_local.py
+++ code/rag_local.py
@@ -12,6 +12,7 @@ from langchain_core.documents import Document
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from rich import print
 
 from config import CHUNK_OVERLAP, CHUNK_SIZE, DATA_DIR, PERSIST_DIR
